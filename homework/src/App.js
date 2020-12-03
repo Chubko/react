a82bd32d220afb8dd94e701fa91df2e857fa1dd2
Liydmyla Chubko <luda.chubko@gmail.com>
2020-12-03 22:50:55 +0200

    homework2

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import AllCommentsComponent from "./components/komments/All Comments Component";
+import AllCommentsComponent from "./components/comments/All Comments Component";
+import AllPostsComponent from "./components/posts/All Posts Component";
 
 class App extends Component {
     render() {
         return (
             <div>
               <AllCommentsComponent/>
+              <AllPostsComponent/>
             </div>
         );
     }
