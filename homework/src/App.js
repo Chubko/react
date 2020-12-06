df5aae8c6fc5bb2c315b0f01b934fa6aaa6ffa6d
Liydmyla Chubko <luda.chubko@gmail.com>
2020-12-06 18:58:05 +0200

    homework2

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
 import AllCommentsComponent from "./components/comments/All Comments Component";
 import AllPostsComponent from "./components/posts/All Posts Component";
+import AllAlbums from "./components/albums/All albums";
+import AllToDo from "./components/todos/All ToDo";
 
 class App extends Component {
     render() {
         return (
             <div>
-              <AllCommentsComponent/>
-              <AllPostsComponent/>
+              {/*<AllCommentsComponent/>*/}
+              {/*<AllPostsComponent/>*/}
+              {/*<AllAlbums/>*/}
+              <AllToDo/>
             </div>
         );
     }
