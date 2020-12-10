e45ef93519de3d863e6d54dd67d76cbd6c486b60
Liydmyla Chubko <luda.chubko@gmail.com>
2020-12-10 12:44:23 +0200

    homework4

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
+import AllComments from "./components/comments/AllComments";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+import AllPosts from "./components/posts/AllPosts";
+import AllUsers from "./components/users/AllUsers";
+
 
 class App extends Component {
     render() {
         return (
+            <Router>
             <div>
-              
+                <div>
+                    <Link to={'/users'}>
+                        users
+                    </Link>
+                </div>
+                <div>
+                <Link to={'/comments'}>
+                    comments
+                </Link>
+                </div>
+                <div>
+                    <Link to={'/posts'}>
+                        posts
+                    </Link>
+                </div>
+                <Switch>
+                    <Route path={'/users'} render={()=>{return <AllUsers/>}}/>
+                    <Route path={'/comments'} render={()=>{return <AllComments/>}}/>
+                    <Route path={'/posts'} render={()=>{return <AllPosts/>}}/>
+                </Switch>
             </div>
+            </Router>
         );
     }
 }
