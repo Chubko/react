cd57445be018000df86d007041dfb2dede70451d
Liydmyla Chubko <luda.chubko@gmail.com>
2020-12-25 13:32:49 +0200

    homework8

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,13 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {useSelector, useDispatch} from "react-redux";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-              
-            </div>
-        );
-    }
+export default function App(){
+    const counter = useSelector(({counter})=>counter);
+    const todo = useSelector(({todo})=>todo);
+    const dispatch = useDispatch();
+    let inputValue = '';
+
+    const fetchData = async ()=>{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`);
+        const json = await response.json();
+        dispatch({type: 'SET_TODO_ITEM', payload: json});
+    };
+
+    const handlerIncrement = ()=> dispatch({type: 'INC_COUNTER'});
+    const handlerDecrement = ()=> dispatch({type: 'DEC_COUNTER'});
+    const handlerReset = ()=> dispatch({type: 'RESET_COUNTER'});
+    const handleResetToDo = ()=> {dispatch({type: 'RESET_TODO'});};
+    const handleToDoCompleted = ()=> {dispatch({type: 'TOGGLE_TODO_COMPLETED'});};
+    const handleTitleChange = ()=> {dispatch({type: 'CHANGE_TODO_TITLE', payload: inputValue});};
+
+    return(
+        <>
+        <div>
+            <h1>Using Redux</h1>
+            <h2>Counter: {counter}</h2>
+            <button onClick={handlerIncrement}>++</button>
+            <button onClick={handlerDecrement}>--</button>
+            <button onClick={handlerReset}>reset</button>
+            <button onClick={fetchData}>setToDo</button>
+            <button onClick={handleResetToDo}>resetToDo</button>
+            <button onClick={handleToDoCompleted}>toggle</button>
+            <input onChange={({target: {value}})=>inputValue=value}/>
+            <button onClick={handleTitleChange}>change title</button>
+
+            {!!todo && (<h2>{todo.id}-{todo.title}-{todo.completed.toString()}</h2>)}
+        </div>
+        </>
+    )
 }
-
-export default App;
--- homework/src/index.js
+++ homework/src/index.js
@@ -3,10 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState = {counter:1, todo: null};
+const reducer = (state = initialState, action)=>{
+    switch (action.type){
+        case 'INC_COUNTER': {
+            return {...state, counter: state.counter+1}
+        }
+        case 'DEC_COUNTER': {
+            if(state.counter>1)
+            return {...state, counter: state.counter - 1}
+        }
+        case 'RESET_COUNTER': {
+            return {...state, counter: 1}
+        }
+        case 'SET_TODO_ITEM': {
+            return {...state, todo: action.payload};
+        }
+        case 'RESET_TODO': {
+            return {...state, todo: null};
+        }
+        case 'TOGGLE_TODO_COMPLETED': {
+            return {...state, todo: {...state.todo, completed: !state.todo.completed}}
+        }
+        case 'CHANGE_TODO_TITLE': {
+            return {...state, todo: {...state.todo, title: action.payload}}
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
