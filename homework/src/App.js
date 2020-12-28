f97cc296d0154a9203415119c7bdc00b6e00daef
Liydmyla Chubko <luda.chubko@gmail.com>
2020-12-28 20:36:20 +0200

    homework9-internet-shop

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import AppHeader from "./components/AppHeader";
+import ProductList from "./components/ProductList";
+import {BrowserRouter as Router} from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
-            <div>
-              
-            </div>
+            <Router>
+                <div>
+                    <AppHeader/>
+                    <ProductList/>
+                </div>
+            </Router>
         );
     }
 }
